acd3e25d23d1ac1df6336e5fe0441524272746d7
Elvisthacoder <ekk213@gmail.com>
2016-03-15 17:09:32 +0300

    add side menu ammendments

---
+++ splash/app/src/main/java/com/example/martin/cladi/splash.java
@@ -0,0 +1,49 @@
+package com.example.martin.cladi;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+
+/**
+ * Created by martin on 3/6/16.
+ */
+public class splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        final ImageView iv = (ImageView) findViewById(R.id.imageView);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+        final Animation an2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+
+
+        iv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                iv.startAnimation(an2);
+                finish();
+                Intent i = new Intent(this,MainActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+}
